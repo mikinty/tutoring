0bc64b82ae2f0aaeb0b6fcb82c01a0bd2e05cd63
mikinty <youmichaelc@gmail.com>
2022-06-20 23:40:26 -0400

    Styled the headshots tutors page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import "./styles.scss";
+import Classes from "./pages/Classes";
+import Tutors from "./pages/Tutors";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Tutors />
+      <Classes />
+    </div>
+  );
 }
 
 export default App;
