a1d798b56368a699fadebdd936ebed742203283c
mikinty <youmichaelc@gmail.com>
2022-06-21 00:19:35 -0400

    Added headshot linkedin links

--- src/pages/Tutors.js
+++ src/pages/Tutors.js
@@ -2,12 +2,12 @@ import React from "react";
 import myou from "../images/myou.jpg";
 import linxie from "../images/linxie.jpg";
 
-function headshot(image, name, description) {
+function headshot(image, name, description, link) {
   return (
     <div className="headshot column">
-      <div className="headshot-image-wrapper">
+      <a className="headshot-image-wrapper" href={link}>
         <img src={image} alt={`headshot of ${name}`} />
-      </div>
+      </a>
       <h2>{name}</h2>
       <div className="description">{description}</div>
     </div>
@@ -22,12 +22,14 @@ export default function Tutors() {
         {headshot(
           linxie,
           "Lin Xie",
-          "Lin graduated from Cornell University and currently works as a software engineer at Doordash. Lin is a tutor for Rewrite the code, a CS program for girls, and has tutored kids 5-18 as part of Juni learning."
+          "Lin graduated from Cornell University and currently works as a software engineer at Doordash. Lin is a tutor for Rewrite the code, a CS program for girls, and has tutored kids 5-18 as part of Juni learning.",
+          "https://www.linkedin.com/in/lin-xie-1b3411151/"
         )}
         {headshot(
           myou,
           "Michael You",
-          "Michael graduated from Carnegie Mellon with degrees in computer science and electrical engineering, and currently works at Meta (Facebook). Michael has tutored for over 10 years and has written curriculums, supplementary notes and guides on a variety of academic topics."
+          "Michael graduated from Carnegie Mellon with degrees in computer science and electrical engineering, and currently works at Meta (Facebook). Michael has tutored for over 10 years and has written curriculums, supplementary notes and guides on a variety of academic topics.",
+          "https://www.linkedin.com/in/mcyou/"
         )}
       </div>
     </div>
